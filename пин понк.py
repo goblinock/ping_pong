d6abc7c01755bcf65dd68ab96d36db65611eb122
goblinock <alex.sukhanov.08122010@gmail.com>
2025-04-12 18:40:27 +0500

    Add files via upload

--- пин понк.py
+++ пин понк.py
@@ -1,95 +1,60 @@
 from pygame import *
-# from random import randint
-# from time import time as timer
 window = display.set_mode((700, 500))
-# display.set_caption('пин понг')
-# background = transform.scale(image.load("galaxy.jpg"),(700, 500))
+display.set_caption('пинг-понг')
 window.fill((12, 32, 45))
 clock = time.Clock()
 FPS = 60
 
-# mixer.init()
-# mixer.music.load('space.ogg')
-# mixer.music.play()
-# fire = mixer.Sound('fire.ogg')
-# # fire.play()
 
-# score = 0
+score = 0
 
-# class GameSprite(sprite.Sprite):
-#     def __init__(self, player_image, player_x, player_y, player_speed,size_x, size_y):
-#         super().__init__()
-#         self.image = transform.scale(image.load(player_image),(size_x, size_y))
-#         self.speed = player_speed
-#         self.rect = self.image.get_rect()
-#         self.rect.x = player_x
-#         self.rect.y = player_y
-#     def reset(self):
-#         window.blit(self.image, (self.rect.x, self.rect.y))
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, player_speed,size_x, size_y):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image),(size_x, size_y))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
 
 
-# class Enemy(GameSprite):
-#     def update(self):
-#         self.rect.y += self.speed
-#         if self.rect.y > 500:
-#            self.rect.y = 0
-#            self.rect.x = randint (50, 650)
-        
-        
-
-# lost = 0
-# class Enemy(GameSprite):
-#     def update(self):
-#         self.rect.y += self.speed
-#         global lost
-#         if self.rect.y > 500:
-#             self.rect.x = randint(80, 620)
-#             self.rect.y = 0
-#             lost = lost + 1
+lost = 0
          
-    
+
+class Kirpich_player(GameSprite):
+    def update(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < 400:
+            self.rect.y += self.speed
+    def update_l(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < 400:
+            self.rect.y += self.speed
 
 
-# class Player(GameSprite):
-#     def update(self):
-#         keys = key.get_pressed()
-#         if keys[K_LEFT] and self.rect.x > 5:
-#             self.rect.x -= self.speed
-#         if keys[K_RIGHT] and self.rect.x < 620:
-#             self.rect.x += self.speed
-
-
-#     def fire(self):
-#         bullet1 = Bullet(('bullet.png'), self.rect.centerx, self.rect.top, -15, 20, 15)
-#         bullets.add(bullet1)
  
 
-# class Bullet(GameSprite):
-#     def update(self):
-#         self.rect.y += self.speed
-#         if self.rect.y < 0:
-#             self.kill()
 
 
-# ship = Player('rocket.png', 5, 400, 10, 80, 100)
+
+
+kirpich1 = Kirpich_player('kirpich1.png', 615, 100, 15, 80, 150)
+kirpich2 = Kirpich_player('kirpich2.png', 5, 100, 15, 80, 150)
+kirpich3 = GameSprite('kirpich3.png', 250, 250, 15, 150, 150)
 
 
 
 game = True
-# finish = False
-
-# asteroids = sprite.Group()
-
-# for i in range(1, 3):
-#     asteroid = Enemy('asteroid.png', randint(80, 620), -40, randint(1, 3), 80, 50)
-#     asteroids.add(asteroid)
+finish = False
 
 
-# monsters = sprite.Group()
 
-# for i in range(1, 3):
-#     monster = Enemy('ufo.png',randint(80, 620), -40, randint(1, 3), 80, 50)
-#     monsters.add(monster)
    
 # font.init()
 # font1 = font.SysFont('Arial', 36)
@@ -103,39 +68,46 @@ game = True
 
 while game:
     
-#     for e in event.get():
-#         if e.type == QUIT:
-#             game = False
-#         elif e.type == KEYDOWN:
-#             if e.key == K_SPACE:
-#                 if num_fire < 5 and rel_time == False:
-#                     num_fire = num_fire +1
-#                     fire.play()
-#                     ship.fire()
-#                 if num_fire >=5 and rel_time == False:
-#                     last_time = timer()
-#                     rel_time = True
-                
-#     if finish != True:
-#         window.blit(background,(0,0))
-#         ship.reset()
-#         ship.update()
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
 
+                
+
+
+
+    if finish != True:
+        # if move_right:
+        #     platform.rect.x += 3
+        # if move_left:
+        #     platform.rect.x -= 3
+        # ball.rect.x += speed_x
+        # ball.rect.y += speed_y
+        # if ball.rect.colliderect(platform.rect):
+        #     speed_y *= -1
+        # if ball.rect.y < 0:
+        #     speed_y *= -1
+        # if ball.rect.x < 0 or ball.rect.x > 450:
+        #     speed_x *= -1
+        # if ball.rect.y > 420:
+        window.fill((12, 32, 45))
+        kirpich1.reset()
+        kirpich1.update()
+    
+
+        kirpich2.reset()
+        kirpich2.update_l()
+
+
+        kirpich3.reset()
+        kirpich3.update()
 #         text = font1.render('Счет:' + str(score), 1, (255, 255, 255))
 #         window.blit(text, (10, 20))
 
 #         text_lose = font1.render('Пропущено: ' + str(lost), 1, (255, 255, 255))
 #         window.blit(text_lose, (10, 50))
 
-#         asteroids.draw(window)
-#         asteroids.update()
-
-#         monsters.draw(window)
-#         monsters.update()
-
-#         bullets.draw(window)
-#         bullets.update()
-
+#         
 #         ship.reset()
 #         monsters.draw(window)
 #         bullets.draw(window)
@@ -170,26 +142,6 @@ while game:
 #         if score >= 10 :
 #             finish = True
 #             window.blit(win, (200, 200))
-#     else:
-#         finish = False
-#         score = 0
-#         lost = 0
-#         rel_time = False
-#         num_fire = 0 
-#         for b in bullets:
-#             b.kill()
-#         for m in monsters:
-#             m.kill()
-#         for a in asteroids:
-#             a.kill()
-
-#         time.delay (3000)
-#         for i in range(1, 6):
-#             monster = Enemy('ufo.png', randint(80, 620), -40, randint(1, 3), 80, 50)
-#             monsters.add(monster)
-#         for i in range(1, 3):
-#             asteroid = Enemy('asteroid.png', randint(80, 620), -40, randint(1, 3), 80, 50)
-#             asteroids.add(asteroid)
 
       
     clock.tick(FPS)
